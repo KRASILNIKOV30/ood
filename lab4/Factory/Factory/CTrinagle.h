4549541897387687c99205fef8c82609b60e58b7
krasilnikov30 <bkrasilnikov3006@gmail.com>
2023-10-03 08:09:52 +0300

    Добавил треугольник

---
+++ lab4/Factory/Factory/CTrinagle.h
@@ -0,0 +1,39 @@
+#pragma once
+#include "CShape.h"
+
+class CTriangle final : public CShape
+{
+public:
+	CTriangle(const Point& vertex1, const Point& vertex2, const Point& vertex3, const Color color)
+		: CShape(color),
+		  m_vertex1(vertex1),
+		  m_vertex2(vertex2),
+		  m_vertex3(vertex3)
+	{
+	}
+
+	Point GetVertex1() const
+	{
+		return m_vertex1;
+	}
+
+	Point GetVertex2() const
+	{
+		return m_vertex2;
+	}
+
+	Point GetVertex3() const
+	{
+		return m_vertex3;
+	}
+
+private:
+	void DrawLayout(ICanvas& canvas) override
+	{
+		canvas.DrawLine(m_vertex1, m_vertex2);
+		canvas.DrawLine(m_vertex2, m_vertex3);
+		canvas.DrawLine(m_vertex3, m_vertex1);
+	}
+
+	Point m_vertex1, m_vertex2, m_vertex3;
+};
