0f7255581976987bef54792cffc5ab7c9be78030
Bogdan Krasilnikov <bogdan.krasilnikov@ispring.com>
2024-10-18 18:41:41 +0300

    Начал внедрять CMake

---
+++ lab5_cmake/headers/Menu.h
@@ -0,0 +1,16 @@
+//
+// Created by bogdan.krasilnikov on 18.10.24.
+//
+
+#ifndef MENU_H
+#define MENU_H
+
+
+
+class Menu {
+
+};
+
+
+
+#endif //MENU_H
---
+++ lab5_cmake/sources/Menu.cpp
@@ -0,0 +1,5 @@
+//
+// Created by bogdan.krasilnikov on 18.10.24.
+//
+
+#include "Menu.h"
