1e846f796203cc30ee543d9a5f4b7e1cace0fa6b
krasilnikov30 <bkrasilnikov3006@gmail.com>
2024-11-14 19:50:55 +0300

    Удалил лишнюю папку

--- cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/bogdan.krasilnikov/projects/ood
-# Build directory: /home/bogdan.krasilnikov/projects/ood/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("lab5_cmake")
--- cmake-build-debug/lab5_cmake/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/bogdan.krasilnikov/projects/ood/lab5_cmake
-# Build directory: /home/bogdan.krasilnikov/projects/ood/cmake-build-debug/lab5_cmake
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(HtmlEditor_tests "/home/bogdan.krasilnikov/projects/ood/cmake-build-debug/lab5_cmake/HtmlEditor_tests")
-set_tests_properties(HtmlEditor_tests PROPERTIES  _BACKTRACE_TRIPLES "/home/bogdan.krasilnikov/projects/ood/lab5_cmake/CMakeLists.txt;22;add_test;/home/bogdan.krasilnikov/projects/ood/lab5_cmake/CMakeLists.txt;0;")
